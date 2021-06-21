f528779728fa5cb107d110017a44bdfea76f834f
nitheeshmavila <mail2nitheeshmavila@gmail.com>
2021-06-21 09:13:11 +0530

    uncomment

--- easy/strings/caesar_cipjer_encripter.py
+++ easy/strings/caesar_cipjer_encripter.py
@@ -1,13 +1,13 @@
 
 # O(N) space 
 # O(N) time, Where N is the no of chars in the string
-# def caesarCipherEncryptor(string, key):
-#     alphabets = list('abcdefghijklmnopqrstuvwxyz')
-#     encrypted = []
-#     for ch in string:
-#         new_index = (alphabets.index(ch) + key) % 26
-#         encrypted.append(alphabets[new_index])
-#     return "".join(encrypted)
+def caesarCipherEncryptor(string, key):
+    alphabets = list('abcdefghijklmnopqrstuvwxyz')
+    encrypted = []
+    for ch in string:
+        new_index = (alphabets.index(ch) + key) % 26
+        encrypted.append(alphabets[new_index])
+    return "".join(encrypted)
 
 
 def caesarCipherEncryptor(string, key):
@@ -15,8 +15,8 @@ def caesarCipherEncryptor(string, key):
     for ch in string:
         new_code = ord(ch) + key
         unicode = new_code
-        if new_code >= 122:
-            unicode = 96 + (new_code% 122)
+        if new_code <= 122:
+            unicode = 96 + (new_code % 122)
 
         encrypted.append(chr(unicode))
     return "".join(encrypted)
